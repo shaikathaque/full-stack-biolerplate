0c72142e15a99f5651f83bd6ac23aaf7937f05d4
shaikathaque <shaikathaque4@gmail.com>
2019-03-22 00:54:06 -0400

    serve production client build and redirect react router requests to index.html

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 app.use(morgan('dev'));
 
+// in production, serve build when user requests path '/'.
+app.use(express.static('../client/build'));
+
 require('./routes/helloRoutes')(app);
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../client/build/index.html')),);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
